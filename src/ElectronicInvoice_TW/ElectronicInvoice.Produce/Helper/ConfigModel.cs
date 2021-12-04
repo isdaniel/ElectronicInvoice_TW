0f8441b581332b57590ecde60e412f333a6abe4f
DanielShih <dog830228@gmail.com>
2021-12-05 00:15:01 +0800

    對於 Ndepend 提供修改

---
+++ src/ElectronicInvoice_TW/ElectronicInvoice.Produce/Helper/ConfigModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ElectronicInvoice.Produce.Helper
+{
+    internal class ConfigModel
+    {
+        public string GovAppId { get; set; }
+        public string GovAPIKey { get; set; }
+        public string IsMockAPI { get; set; }
+        public Dictionary<string, string> ApiURLTable { get; set; }
+    }
+}
